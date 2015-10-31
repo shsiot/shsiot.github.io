40bc15113c1323b3efceceee4fd1c0e28407e92e
shsiot <shsiotclub@gmail.com>
2015-10-31 10:46:48 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,8 +108,8 @@
 		(function() {
 
 			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+				var $form = document.querySelectorAll('signup-form')[0],
+					$submit = document.querySelectorAll('signup-form input[type="submit"]')[0],
 					$message;
 
 			// Bail if addEventListener isn't supported.
